722cd98c3bf711aa75b30e00a329d3fccbba0658
bk1654512-maker <bk1654512@gmail.com>
2026-08-15 21:45:57 +0500

    Add RationalNumber class with numerator and denominator

---
+++ rational numbers.java
@@ -0,0 +1,18 @@
+public class RationalNumber {
+
+    private int numerator;
+    private int denominator;
+
+    public RationalNumber(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+}
